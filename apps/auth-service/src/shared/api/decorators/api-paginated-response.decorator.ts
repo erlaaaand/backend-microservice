4d3b7c357a7d913adce51be7252066a3e1edf844
erlaaaand <erlandagsya2@gmail.com>
2025-12-14 06:46:11 +0700

    feat(api-paginatend-response)

---
+++ apps/auth-service/src/shared/api/decorators/api-paginated-response.decorator.ts
@@ -0,0 +1,39 @@
+// apps/auth-service/src/shared/api/decorators/api-paginated-response.decorator.ts
+
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+
+/**
+ * Swagger decorator untuk paginated response
+ */
+export const ApiPaginatedResponse = <TModel extends Type<any>>(
+    model: TModel,
+) => {
+    return applyDecorators(
+        ApiExtraModels(model),
+        ApiOkResponse({
+            description: 'Paginated response',
+            schema: {
+                allOf: [
+                    {
+                        properties: {
+                            data: {
+                                type: 'array',
+                                items: { $ref: getSchemaPath(model) },
+                            },
+                            meta: {
+                                type: 'object',
+                                properties: {
+                                    total: { type: 'number' },
+                                    page: { type: 'number' },
+                                    limit: { type: 'number' },
+                                    totalPages: { type: 'number' },
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
+    );
+};
