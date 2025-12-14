00a7d5450deb6bce8bc77355a8cd66156191e968
erlaaaand <erlandagsya2@gmail.com>
2025-12-14 14:59:59 +0700

    update redis module

--- apps/auth-service/src/shared/infrastructure/caching/redis.module.ts
+++ apps/auth-service/src/shared/infrastructure/caching/redis.module.ts
@@ -1,12 +1,10 @@
-// Update: apps/auth-service/src/shared/infrastructure/caching/redis.module.ts
-
 import { Global, Module, Provider } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
-import { getRedisConfig } from '../config/redis.config';
+import { getRedisConfig, REDIS_CLIENT } from '../config/redis.config';
 import { CacheService } from './cache.service';
 
-export const REDIS_CLIENT = 'REDIS_CLIENT';
+// [HAPUS BARIS INI] export const REDIS_CLIENT = 'REDIS_CLIENT';
 
 const redisProvider: Provider = {
     provide: REDIS_CLIENT,
@@ -18,7 +16,8 @@ const redisProvider: Provider = {
 
 @Global()
 @Module({
+    imports: [ConfigModule],
     providers: [redisProvider, CacheService],
     exports: [redisProvider, CacheService],
 })
-export class RedisModule { }
+export class RedisModule {}
