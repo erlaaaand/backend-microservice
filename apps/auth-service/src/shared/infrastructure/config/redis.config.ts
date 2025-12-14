a6cd80e67b47bcc06671d10af2bc0a582145217a
erlaaaand <erlandagsya2@gmail.com>
2025-12-14 14:59:38 +0700

    fix redis.config

--- apps/auth-service/src/shared/infrastructure/config/redis.config.ts
+++ apps/auth-service/src/shared/infrastructure/config/redis.config.ts
@@ -1,6 +1,9 @@
 import { ConfigService } from '@nestjs/config';
 import { RedisOptions } from 'ioredis';
 
+// [TAMBAHAN BARU] Pindahkan konstanta ke sini
+export const REDIS_CLIENT = 'REDIS_CLIENT';
+
 export const getRedisConfig = (configService: ConfigService): RedisOptions => ({
     host: configService.get<string>('REDIS_HOST'),
     port: configService.get<number>('REDIS_PORT'),
